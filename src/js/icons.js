df316c48996b54aff7832dc7b954e06091f7f4ac
iROCKBUNNY <irockbunny@outlook.com>
2018-09-13 03:14:20 +0800

    Update

---
+++ src/js/icons.js
@@ -0,0 +1,27 @@
+import play from '../assets/play.svg';
+import pause from '../assets/pause.svg';
+import volumeUp from '../assets/volume-up.svg';
+import volumeDown from '../assets/volume-down.svg';
+import volumeOff from '../assets/volume-off.svg';
+import full from '../assets/full.svg';
+import fullWeb from '../assets/full-web.svg';
+import setting from '../assets/setting.svg';
+import right from '../assets/right.svg';
+import pallette from '../assets/pallette.svg';
+import loading from '../assets/loading.svg';
+
+const Icons = {
+    play: play,
+    pause: pause,
+    volumeUp: volumeUp,
+    volumeDown: volumeDown,
+    volumeOff: volumeOff,
+    full: full,
+    fullWeb: fullWeb,
+    setting: setting,
+    right: right,
+    pallette: pallette,
+    loading: loading,
+};
+
+export default Icons;
---
+++ src/js/template.js
@@ -0,0 +1,70 @@
+import Icons from './icons';
+import tplPlayer from '../templates/player.art';
+
+class Template {
+    constructor (options) {
+        this.container = options.container;
+        this.options = options.options;
+        this.index = options.index;
+        this.tran = options.tran;
+        this.init();
+    }
+
+    init () {
+        this.container.innerHTML = tplPlayer({
+            options: this.options,
+            index: this.index,
+            tran: this.tran,
+            icons: Icons,
+            video: {
+                current: true,
+                pic: this.options.video.pic,
+                preload: this.options.preload,
+                url: this.options.video.url,
+            }
+        });
+
+        this.volumeBar = this.container.querySelector('.y-player-volume-bar-inner');
+        this.volumeBarWrap = this.container.querySelector('.y-player-volume-bar');
+        this.volumeBarWrapWrap = this.container.querySelector('.y-player-volume-bar-wrap');
+        this.volumeButton = this.container.querySelector('.y-player-volume');
+        this.volumeButtonIcon = this.container.querySelector('.y-player-volume-icon');
+        this.volumeIcon = this.container.querySelector('.y-player-volume-icon .y-player-icon-content');
+        this.playedBar = this.container.querySelector('.y-player-played');
+        this.loadedBar = this.container.querySelector('.y-player-loaded');
+        this.playedBarWrap = this.container.querySelector('.y-player-bar-wrap');
+        this.playedBarTime = this.container.querySelector('.y-player-bar-time');
+        this.video = this.container.querySelector('.y-player-video-current');
+        this.bezel = this.container.querySelector('.y-player-bezel-icon');
+        this.playButton = this.container.querySelector('.y-player-play-icon');
+        this.videoWrap = this.container.querySelector('.y-player-video-wrap');
+        this.controllerMask = this.container.querySelector('.y-player-controller-mask');
+        this.ptime = this.container.querySelector('.y-player-ptime');
+        this.settingButton = this.container.querySelector('.y-player-setting-icon');
+        this.settingBox = this.container.querySelector('.y-player-setting-box');
+        this.mask = this.container.querySelector('.y-player-mask');
+        this.speed = this.container.querySelector('.y-player-setting-speed');
+        this.speedItem = this.container.querySelectorAll('.y-player-setting-speed-item');
+        this.dtime = this.container.querySelector('.y-player-dtime');
+        this.controller = this.container.querySelector('.y-player-controller');
+        this.browserFullButton = this.container.querySelector('.y-player-full-icon');
+        this.webFullButton = this.container.querySelector('.y-player-full-in-icon');
+        this.menu = this.container.querySelector('.y-player-menu');
+        this.menuItem = this.container.querySelectorAll('.y-player-menu-item');
+        this.qualityList = this.container.querySelector('.y-player-quality-list');
+        this.qualityButton = this.container.querySelector('.y-player-quality-icon');
+        this.barPreview = this.container.querySelector('.y-player-bar-preview');
+        this.barWrap = this.container.querySelector('.y-player-bar-wrap');
+        this.notice = this.container.querySelector('.y-player-notice');
+        this.infoPanel = this.container.querySelector('.y-player-info-panel');
+        this.infoPanelClose = this.container.querySelector('.y-player-info-panel-close');
+        this.infoVersion = this.container.querySelector('.y-player-info-panel-item-version .y-player-info-panel-item-data');
+        this.infoFPS = this.container.querySelector('.y-player-info-panel-item-fps .y-player-info-panel-item-data');
+        this.infoType = this.container.querySelector('.y-player-info-panel-item-type .y-player-info-panel-item-data');
+        this.infoUrl = this.container.querySelector('.y-player-info-panel-item-url .y-player-info-panel-item-data');
+        this.infoResolution = this.container.querySelector('.y-player-info-panel-item-resolution .y-player-info-panel-item-data');
+        this.infoDuration = this.container.querySelector('.y-player-info-panel-item-duration .y-player-info-panel-item-data');
+    }
+}
+
+export default Template;
